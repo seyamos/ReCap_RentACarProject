bb64d559a1664c7a8a1e3f2cf8e7c1a42fae32b6
seydaerbay <seydaerbay@outlook.com>
2021-02-02 17:55:03 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,58 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            Console.WriteLine("***Tüm Araçların Listesi***");
+            
+            foreach (var car in carManager.GetAll())
+            {                
+                Console.WriteLine("Araç ID: " + car.Id + ", Marka ID: " + car.BrandId + ", Model Yılı: " + car.ModelYear + ", Renk ID: "  + car.ColorId + ", Günlük Kirası: " + car.DailyPrice + ", Açıklama: " + car.Description);
+            }
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("***Yeni Bir Aracın Eklenmesi ve Araçların Yeniden Listelenmesi***");
+            Car car6 = new Car() { Id = 6, BrandId = 3, ColorId = 4, ModelYear = 2022, DailyPrice = 900, Description = "     0 km" };            
+            carManager.Add(car6);            
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç ID: " + car.Id + ", Marka ID: " + car.BrandId + ", Model Yılı: " + car.ModelYear + ", Renk ID: " + car.ColorId + ", Günlük Kirası: " + car.DailyPrice + ", Açıklama: " + car.Description);
+            }
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("***Id No'su 1 Olan Aracın Güncellenmesi ve Araçların Yeniden Listelenmesi***");
+            //Güncellenecek Araç: Id=1,BrandId=1,ColorId=1,ModelYear=2005,DailyPrice=200,Description=" 50000 km'de            
+            Car car1 = new Car { Id = 1, BrandId = 1, ColorId = 1, ModelYear = 2005, DailyPrice =185, Description=" 50000 km'deki aracın fiyatı güncellendi"};
+            carManager.Update(car1);            
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç ID: " + car.Id + ", Marka ID: " + car.BrandId + ", Model Yılı: " + car.ModelYear + ", Renk ID: " + car.ColorId + ", Günlük Kirası: " + car.DailyPrice + ", Açıklama: " + car.Description);
+            }
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("***Id No'su 2 Olan Aracın Silinmesi ve Araçların Yeniden Listelenmesi***");
+            Car car2 = new Car { Id = 2 };
+            carManager.Delete(car2);
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araç ID: " + car.Id + ", Marka ID: " + car.BrandId + ", Model Yılı: " + car.ModelYear + ", Renk ID: " + car.ColorId + ", Günlük Kirası: " + car.DailyPrice + ", Açıklama: " + car.Description);
+
+            }
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("***Id No'su 3 Olan Aracın Diğer Bilgilerinin Getirilmesi***");
+            Car car3 = carManager.GetById(3);
+            Console.WriteLine("Araç Bilgileri: " + "Araç ID: " + car3.Id + "; Marka ID=" + car3.BrandId + "; Model Yıllı=" + car3.ModelYear + "; Renk ID=" + car3.ColorId + "; Günlük Kirası=" + car3.DailyPrice + "; Açıklaması=" + car3.Description);
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;  //global dğşkn
+        public InMemoryCarDal()  //constructor : void yok , birşey döndürmüyor, class ın ismiyle tanımlı:bu contrc dır. ctor tab tab il e yapılır
+        {
+            _cars = new List<Car> {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2005,DailyPrice=200,Description=" 50000 km"},
+                new Car{Id=2,BrandId=1,ColorId=1,ModelYear=2010,DailyPrice=300,Description=" 30000 km"},
+                new Car{Id=3,BrandId=1,ColorId=2,ModelYear=2015,DailyPrice=400,Description=" 20000 km"},
+                new Car{Id=4,BrandId=2,ColorId=2,ModelYear=2020,DailyPrice=500,Description=" 10000 km"},
+                new Car{Id=5,BrandId=2,ColorId=3,ModelYear=2021,DailyPrice=600,Description="  5000 km"},
+            };
+                
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)  //LINQ : Language Integrated Query
+        {
+            Car carToDelete = _cars.Where(c=> c.Id == car.Id).SingleOrDefault();    //(system linq) => bu işarete lambda denir
+
+            _cars.Remove(carToDelete);
+            
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+        
+        
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+
+        Car ICarDal.GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).SingleOrDefault();
+        }
+    }
+}
