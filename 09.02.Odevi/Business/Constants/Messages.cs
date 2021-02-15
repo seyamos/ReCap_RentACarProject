58d48aa27918b4883f762a2296cb7f53240281eb
seydaerbay <seydaerbay@outlook.com>
2021-02-16 00:18:24 +0300

    9 Şubat Ödevi-1

---
+++ 09.02.Odevi/Business/Constants/Messages.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages  //static yaparsak new lememize gerek kalmaz
+    {
+        public static string CarAdded = "Araç eklendi";
+        public static string CarDailyPriceInvalid = "Araç fiyatı geçersiz.Girilen fiyat 0'dan büyük olmalıdır";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string CarsListed="Araçlar listelendi";
+        public static string CarNameInvalid = "Araç ismi geçersiz. Araç ismi en az 2 karakter olmalıdır";
+        public static string BrandNameInvalid = "Marka ismi geçersiz. Marka ismi en az 2 karakter olmalıdır";
+        public static string BrandAdded = "Marka Eklendi";
+        public static string BrandsListed = "Markalar Listelendi";
+        public static string ColorAdded = "Renk Eklendi";
+        public static string ColorsListed = "Renkler Listelendi";
+        //public static string BrandNameInvalid = "Araç ismi geçersiz";  //CarName ekleyerek DB'ye burayı düzelt kendime not.
+    }
+}
---
+++ 09.02.Odevi/DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,35 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //NuGet
+    public class EfCarDal : EfEntityRepositoryBase<Car, RentACarContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (RentACarContext context =new RentACarContext())
+            {
+                var result = from c in context.Cars
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             join y in context.Colors
+                             on c.ColorId equals y.ColorId
+                             select new CarDetailDto 
+                             {
+                                 CarId = c.CarId, CarName=c.CarName, BrandName=b.BrandName, ColorName=y.ColorName, ModelYear=c.ModelYear, Descriptions=c.Descriptions, DailyPrice=c.DailyPrice
+                             };
+                return result.ToList();
+
+            }
+        }
+    }
+}
