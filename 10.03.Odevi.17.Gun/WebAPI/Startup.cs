84a87fb36f6ef121f37f14e35fb2ecc229a29e3f
seydaerbay <seydaerbay@outlook.com>
2021-03-18 00:37:21 +0300

    local host added

--- 10.03.Odevi.17.Gun/WebAPI/Startup.cs
+++ 10.03.Odevi.17.Gun/WebAPI/Startup.cs
@@ -95,6 +95,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());  //frontend için**************
 
             app.UseHttpsRedirection();
