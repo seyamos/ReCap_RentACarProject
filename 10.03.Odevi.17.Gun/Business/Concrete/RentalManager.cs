b629ecd22b6fc8cf61ec98bfa9da5207375daf88
seydaerbay <seydaerbay@outlook.com>
2021-03-18 00:30:06 +0300

    join Rental context added local host added Thread.Sleep added(in cotrollers)

---
+++ 10.03.Odevi.17.Gun/Business/Concrete/RentalManager.cs
@@ -0,0 +1,87 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        [ValidationAspect(typeof(RentalValidator))]
+        public IResult Add(Rental rental)
+        {
+            IResult result = BusinessRules.Run(CheckIfUndeliveredCar(rental.CarId));
+
+            if (result != null)
+            {
+                return result;
+            }
+
+            _rentalDal.Add(rental);
+            return new SuccessResult(Messages.RentalAdded);
+            
+            
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
+        }
+
+        public IDataResult<Rental> GetById(int rentalId)
+        {
+            return new SuccessDataResult<Rental>(_rentalDal.Get(r => r.RentalId == rentalId));
+        }
+
+        public IDataResult<List<RentalDetailDto>> GetRentalDetails()
+        {
+            return new SuccessDataResult<List<RentalDetailDto>>(_rentalDal.GetRentalDetails());
+        }
+
+        public IResult Update(Rental rental)
+        {
+            IResult result = BusinessRules.Run(CheckIfUndeliveredCar(rental.CarId));
+
+            if (result != null)
+            {
+                return result;
+            }
+
+            _rentalDal.Update(rental);
+            return new SuccessResult(Messages.RentalUpdated);
+        }
+
+
+
+        private IResult CheckIfUndeliveredCar(int carId)    //Rental rental da yazıp aşağıda rental.carId de bırakabilirsin istersen- veya bu şekilde int carId da diyeblirsin
+        {
+            var result = _rentalDal.GetAll(r => r.CarId == carId && r.ReturnDate == null);
+            if (result.Count > 0)
+            {
+                return new ErrorResult(Messages.UndeliveredCar);
+            }
+            return new SuccessResult();
+        }
+    }
+}
---
+++ 10.03.Odevi.17.Gun/Business/Constants/Messages.cs
@@ -0,0 +1,55 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages  //static yaparsak new lememize gerek kalmaz
+    {
+        public static string CarAdded = "Araç eklendi";
+        public static string CarDailyPriceInvalid = "Araç fiyatı geçersiz.Girilen fiyat 0'dan büyük olmalıdır";        
+        public static string CarsListed="Araçlar listelendi";
+        public static string CarNameInvalid = "Araç ismi geçersiz. Araç ismi en az 2 karakter olmalıdır";
+
+        public static string BrandNameInvalid = "Marka ismi geçersiz. Marka ismi en az 2 karakter olmalıdır";
+        public static string BrandAdded = "Marka Eklendi";
+        public static string BrandsListed = "Markalar Listelendi";
+
+        public static string ColorAdded = "Renk Eklendi";
+        public static string ColorsListed = "Renkler Listelendi";
+
+        public static string RentalAdded = "Araç Kiralama gerçekleşti.";
+        public static string RentalUpdated = "Araç Kiralama güncellendi.";
+        public static string UndeliveredCar = "Bu araç henüz teslim edilmediği için kiralanamaz.";
+        
+
+        public static string UserAdded = "Kullanıcı eklendi";
+        public static string UsersListed = "Kullanıcılar Listelendi";
+
+        public static string CustomerAdded = "Müşteri eklendi";
+        public static string CustomerListed = "Müşteriler Listelendi";
+
+        public static string CarImageListed = "Resim Listelendi";
+        public static string CarImageAdded = "Resim eklendi";        
+        public static string CarImageDeleted = "Resim silindi";
+        public static string CarImageUpdated = "Resim güncellendi";
+        public static string CarImageLimitExceeded = "Resim sayısı en fazla 5 olabilir";
+
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+
+        public static string UserRegistered = "Kullanıcı başarıyla kayıt oldu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Parola hatalı.";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı.";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut.";
+        public static string AccessTokenCreated = "Access Token başarıyla oluşturuldu.";
+
+
+
+        //public static string BrandNameInvalid = "Araç ismi geçersiz";  //CarName ekleyerek DB'ye burayı düzelt kendime not.
+    }
+}
+
