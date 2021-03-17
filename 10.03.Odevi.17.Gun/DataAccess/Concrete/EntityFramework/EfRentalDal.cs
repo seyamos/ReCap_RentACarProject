fa3c892fac338d264200a03fb42802bf71ed0f53
seydaerbay <seydaerbay@outlook.com>
2021-03-18 00:35:19 +0300

    join rental context

--- 10.03.Odevi.17.Gun/DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ 10.03.Odevi.17.Gun/DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -32,6 +32,7 @@ namespace DataAccess.Concrete.EntityFramework
                                  RentDate = r.RentDate,
                                  ReturnDate = r.ReturnDate
                              };
+
                 return result.ToList();
 
             }
